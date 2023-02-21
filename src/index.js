2285139f4d5ef7c07271ad5af7c2560d453abc1f
Abraham <brhmdeen@gmail.com>
2023-02-21 12:16:11 +0100

    store setup and analytics

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import ReactGA from 'react-ga';
+ReactGA.initialize("G-LW3WXDY2N5");
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App/>);
 
